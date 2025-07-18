bddd60481c760858864e60fd37011a8db76504e2
granthuang999 <66244223+granthuang999@users.noreply.github.com>
2025-07-18 13:38:10 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -6,6 +6,14 @@ import { siteConfig } from '@/lib/config'
  */
 export default function PoweredBy(props) {
   return (
-
+    <div className={`inline text-sm font-serif ${props.className || ''}`}>
+      <span className='mr-1'>Powered by</span>
+      <a
+        href='https://www.futuremedia.work'
+        className='underline justify-start'>
+        NotionNext {siteConfig('VERSION')}
+      </a>
+      .
+    </div>
   )
 }
