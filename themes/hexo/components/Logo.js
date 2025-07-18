ea99125654bf9fc01ebbecc222361f3a5b51f59c
granthuang999 <66244223+granthuang999@users.noreply.github.com>
2025-07-18 14:32:58 +0800

    Update Logo.js

--- themes/hexo/components/Logo.js
+++ themes/hexo/components/Logo.js
@@ -9,14 +9,21 @@ import Link from 'next/link'
 const Logo = props => {
   const { siteInfo } = props
   return (
-    <Link href='/' passHref legacyBehavior>
-      <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
-        <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'>
-          {' '}
-          {siteInfo?.title || siteConfig('TITLE')}
-        </div>
-      </div>
-    </Link>
+<Link href='/' passHref legacyBehavior>
+  <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
+    <div className='p-1.5 rounded transform duration-200 hover:scale-105'>
+      <Image
+        src='/images/futuremedia_logo_120x40.png'
+        alt='未来传媒 - Future Media'
+        width={120}
+        height={40}
+        className='dark:invert'
+        priority
+      />
+    </div>
+  </div>
+</Link>
+
   )
 }
 export default Logo
